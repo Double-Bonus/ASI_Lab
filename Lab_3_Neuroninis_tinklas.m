d5cfed2de2e224fa31b73f7fa82440d57dbba0fa
Zygimantas Marma <zygimantasmarma@gmail.com>
2020-10-06 00:06:22 +0300

    wtf

--- Lab_3_Neuroninis_tinklas.m
+++ Lab_3_Neuroninis_tinklas.m
@@ -1,6 +1,6 @@
 close all, clear
 
-rng(20);
+%rng(20);
 load Lab1Duomenys.txt
 neuron_cnt = 30;
 koef = 0.2; % coeeficent of what percentage is used for test data
@@ -26,11 +26,12 @@ y = net(INdataTEST);  %cia irgi
 perf = perform(net,y,OUTdataTEST) % Suskai?iuojam tikslumo kriterij? / paklaid?
 
 %Get data our Neuron networks' calculation 
-INw=net.IW{1} % iejimo sluoksnio svoriai
-HLw=net.LW{2} % išejimo sluoksnio svoriai
+INputWeigts=net.IW{1} % iejimo sluoksnio svoriai
+HiddenLayerWeigts=net.LW{2} % išejimo sluoksnio svoriai
 B1w=net.b{1} % biaso jungties iejimo sluoksniui svoris
 B2w=net.b{2} % biaso jungties pasl?ptam sluoksniui svoris
  
+
 % randam help dokumentacijoje, kad 
 % a = tansig(n) = 2/(1+exp(-2*n))-1
 % a - funkcijos tansig outputas
@@ -43,25 +44,27 @@ B2w=net.b{2} % biaso jungties pasl?ptam sluoksniui svoris
 Y1 = mapminmax('apply', TESTdata(:,2)', PSmap1);
 Y2 = mapminmax('apply', TESTdata(:,3)', PSmap2);
 Iejimas=[Y1; Y2];  %cia blogai?????
-for j = 1:2
+Iejimas = Iejimas';
+
     for i=1:length(TESTdata')                               % per ??jimus
-        in=Iejimas(j,i);
-        for ii=1:length(INw)                     % per svorius INputo sluoksnio ir bias nepamirstam
-            in_tmp=in*INw(ii,j)+B1w(ii);  
-            inwin(ii)=2/(1+exp(-2*in_tmp))-1;       % per tansig funkcija
+        in=Iejimas(i,:);
+        for ii=1:length(INputWeigts)                     % per svorius INputo sluoksnio ir bias nepamirstam
+            in_tmp=in.*INputWeigts(ii,:)+B1w(ii);  
+            inwin(ii,1)=2/(1+exp(-2*in_tmp(1)))-1;       % per tansig funkcija
+            inwin(ii,2)=2/(1+exp(-2*in_tmp(2)))-1;
         end
-        for ih=1:length(HLw)                        % per svorius pasl?pto sluoksnio
-            h_tmp=inwin(ih)*HLw(ih);
-            inwhl(ih)=h_tmp;                        % nes perdavimo funkcija tiesine
+        for ih=1:length(HiddenLayerWeigts)                        % per svorius pasl?pto sluoksnio
+            h_tmp=inwin(ih,:)*HiddenLayerWeigts(ih);
+            inwhl(ih,1)=h_tmp(1);                        % nes perdavimo funkcija tiesine
+            inwhl(ih,2)=h_tmp(2); 
         end    
-        Out1(i)=sum(inwhl)+B2w;                      % nes perdavimo funkcija tiesine viska sumuojam ir pridedam bias
+        Out1(i,1)=sum(inwhl(:,1))+B2w;                      % nes perdavimo funkcija tiesine viska sumuojam ir pridedam bias
+        Out1(i,2)=sum(inwhl(:,2))+B2w;
     end
-    if j < 2
-        Out_tikras1=mapminmax('reverse',Out1,PSmap1);         % atliekam išejimo kintamuju atnormavima
-    else
-        Out_tikras2=mapminmax('reverse',Out1,PSmap2);         % atliekam išejimo kintamuju atnormavima
-    end
-end
+        Out_tikras1=mapminmax('reverse',Out1(:,1),PSmap1);         % atliekam išejimo kintamuju atnormavima
+        Out_tikras2=mapminmax('reverse',Out1(:,2),PSmap2);         % atliekam išejimo kintamuju atnormavima
+
+
 %Out_tikras1=mapminmax('reverse',Out1,PSmap1);         % atliekam išejimo kintamuju atnormavima
 %Out_tikras2=mapminmax('reverse',Out2,PSmap2);         % atliekam išejimo kintamuju atnormavima
 
