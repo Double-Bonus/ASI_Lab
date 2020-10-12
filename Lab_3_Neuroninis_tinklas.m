89f406e1600b8d64242c924c5075d2827f576e32
Zygimantas Marma <zygimantasmarma@gmail.com>
2020-10-12 20:48:06 +0300

    Added different maps

--- Lab_3_Neuroninis_tinklas.m
+++ Lab_3_Neuroninis_tinklas.m
@@ -1,9 +1,8 @@
 close all, clear
 
-%rng(20);
 load Lab1Duomenys.txt
-neuron_cnt = 30;
-koef = 0.2; % coeeficent of what percentage is used for test data
+neuron_cnt = 40;
+koef = 0.3; % coeeficent of what percentage is used for test data
 
 % Divide data training and testing
 ind=randperm(size(Lab1Duomenys,1)); %Gives index in random order !!!!!CAN CHANGE SEED!!!
@@ -26,12 +25,11 @@ y = net(INdataTEST);  %cia irgi
 perf = perform(net,y,OUTdataTEST) % Suskai?iuojam tikslumo kriterij? / paklaid?
 
 %Get data our Neuron networks' calculation 
-INputWeigts=net.IW{1} % iejimo sluoksnio svoriai
-HiddenLayerWeigts=net.LW{2} % išejimo sluoksnio svoriai
+INw=net.IW{1} % iejimo sluoksnio svoriai
+HLw=net.LW{2} % išejimo sluoksnio svoriai
 B1w=net.b{1} % biaso jungties iejimo sluoksniui svoris
 B2w=net.b{2} % biaso jungties pasl?ptam sluoksniui svoris
  
-
 % randam help dokumentacijoje, kad 
 % a = tansig(n) = 2/(1+exp(-2*n))-1
 % a - funkcijos tansig outputas
@@ -39,37 +37,30 @@ B2w=net.b{2} % biaso jungties pasl?ptam sluoksniui svoris
  
 
  
-[~,PSmap1] = mapminmax(TRAINdata(:,2)',-1,1);                     % atliekam iejimo kintam?j? normavim?
-[~,PSmap2] = mapminmax(TRAINdata(:,3)',-1,1);                    % atliekam iejimo kintam?j? normavim?
+[~,PSmap1] = mapminmax(INdataTRAIN(1,:)',-1,1);                     % atliekam iejimo kintam?j? normavim?
+[~,PSmap2] = mapminmax(INdataTRAIN(2,:)',-1,1);                    % atliekam iejimo kintam?j? normavim?
+[~,PSmapOut] = mapminmax(OUTdataTRAIN(1,:)',-1,1);                    % atliekam iejimo kintam?j? normavim?
 Y1 = mapminmax('apply', TESTdata(:,2)', PSmap1);
 Y2 = mapminmax('apply', TESTdata(:,3)', PSmap2);
-Iejimas=[Y1; Y2];  %cia blogai?????
-Iejimas = Iejimas';
+Iejimas=[Y1; Y2];  %cia blogai????? !!!!!!!!!!!
 
     for i=1:length(TESTdata')                               % per ??jimus
-        in=Iejimas(i,:);
-        for ii=1:length(INputWeigts)                     % per svorius INputo sluoksnio ir bias nepamirstam
-            in_tmp=in.*INputWeigts(ii,:)+B1w(ii);  
-            inwin(ii,1)=2/(1+exp(-2*in_tmp(1)))-1;       % per tansig funkcija
-            inwin(ii,2)=2/(1+exp(-2*in_tmp(2)))-1;
+        in1=Iejimas(1,i);
+        in2=Iejimas(2,i);
+        for ii=1:neuron_cnt                      % per svorius INputo sluoksnio ir bias nepamirstam
+            in_tmp=in1*INw(ii,1)+in1*INw(ii,2)+B1w(ii);  
+            inwin(ii)=2/(1+exp(-2*in_tmp))-1;       % per tansig funkcija
         end
-        for ih=1:length(HiddenLayerWeigts)                        % per svorius pasl?pto sluoksnio
-            h_tmp=inwin(ih,:)*HiddenLayerWeigts(ih);
-            inwhl(ih,1)=h_tmp(1);                        % nes perdavimo funkcija tiesine
-            inwhl(ih,2)=h_tmp(2); 
+        for ih=1:length(HLw)                        % per svorius pasl?pto sluoksnio
+            h_tmp=inwin(ih)*HLw(ih);
+            inwhl(ih)=h_tmp;                        % nes perdavimo funkcija tiesine
         end    
-        Out1(i,1)=sum(inwhl(:,1))+B2w;                      % nes perdavimo funkcija tiesine viska sumuojam ir pridedam bias
-        Out1(i,2)=sum(inwhl(:,2))+B2w;
+        Out1(i)=sum(inwhl)+B2w;                      % nes perdavimo funkcija tiesine viska sumuojam ir pridedam bias
     end
-        Out_tikras1=mapminmax('reverse',Out1(:,1),PSmap1);         % atliekam išejimo kintamuju atnormavima
-        Out_tikras2=mapminmax('reverse',Out1(:,2),PSmap2);         % atliekam išejimo kintamuju atnormavima
 
-
-%Out_tikras1=mapminmax('reverse',Out1,PSmap1);         % atliekam išejimo kintamuju atnormavima
+Out_tikras=mapminmax('reverse',Out1,PSmapOut);         % atliekam išejimo kintamuju atnormavima
 %Out_tikras2=mapminmax('reverse',Out2,PSmap2);         % atliekam išejimo kintamuju atnormavima
 
-
-Out_tikras = Out_tikras1+ Out_tikras2;
 
 % Br?žiam paveiksla , kur atvaizduojam abu atsakus
 figure, plot(Out_tikras,'-ob'),hold on, plot(y,'-*r'),grid
