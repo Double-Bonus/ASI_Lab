5d6723af60508c8459465c60552e9866239c72f5
Zygimantas Marma <zygimantasmarma@gmail.com>
2020-10-17 21:44:32 +0300

    added comments

--- Lab_3_Neuroninis_tinklas.m
+++ Lab_3_Neuroninis_tinklas.m
@@ -4,63 +4,57 @@ load Lab1Duomenys.txt
 neuron_cnt = 40;
 koef = 0.3; % coeeficent of what percentage is used for test data
 
-% Divide data training and testing
-ind=randperm(size(Lab1Duomenys,1)); %Gives index in random order
-dalinam=round(size(Lab1Duomenys,1)*koef); %we change porporsion of train and test data 
-TESTdata=Lab1Duomenys(ind(1:dalinam),:); %nuo pirmo iki dalinam indekso
+% Divide data to training and testing
+ind=randperm(size(Lab1Duomenys,1));             %Gives index in random order
+dalinam=round(size(Lab1Duomenys,1)*koef);       %we change porporsion of train and test data 
+TESTdata=Lab1Duomenys(ind(1:dalinam),:);        %from first till dalinam index
 TRAINdata=Lab1Duomenys(ind(dalinam+1:end),:);
 
-% Paskirstome  iejimus (IN) ir isejimus (OUT)
-INdataTEST=TESTdata(:,2:3)'; %iejimas i neuronini tinkla
+% Davide data to inputs (IN) and outputs (OUT)
+INdataTEST=TESTdata(:,2:3)';                    %input to neuron net
 OUTdataTEST=TESTdata(:,4)';
 INdataTRAIN=TRAINdata(:,2:3)';
 OUTdataTRAIN=TRAINdata(:,4)';
     
-% Treniruojam jutikl? uzduodami pasl?pt? neuron? skai?i? 
-net = feedforwardnet(neuron_cnt); % we change number of neurons
+%Train sensor with given neuron count
+net = feedforwardnet(neuron_cnt);
 net = train(net,INdataTRAIN,OUTdataTRAIN);
-% Testuojame su testavimo imtimi t.y. niekada nematyta
-y = net(INdataTEST);  %cia irgi
-perf = perform(net,y,OUTdataTEST) % Suskai?iuojam tikslumo kriterij? / paklaid?
+%Testing with test data witch is never seen by net
+y = net(INdataTEST);
+perf = perform(net,y,OUTdataTEST) %calculate offset
 
-%Get data our Neuron networks' calculation 
-INw=net.IW{1} % iejimo sluoksnio svoriai
-HLw=net.LW{2} % išejimo sluoksnio svoriai
-B1w=net.b{1} % biaso jungties iejimo sluoksniui svoris
-B2w=net.b{2} % biaso jungties pasl?ptam sluoksniui svoris
+%Get data of our neuron networks' parameters 
+INw=net.IW{1}           % input layer weights
+HLw=net.LW{2}           % output layer weights
+B1w=net.b{1}            % bias input weight
+B2w=net.b{2}            % bias juncture hidden layer weight
  
-% randam help dokumentacijoje, kad 
+%tansing function is implemented as:
 % a = tansig(n) = 2/(1+exp(-2*n))-1
-% a - funkcijos tansig outputas
-% n - funkcijos tansig inputas
  
+[~,PSmapIn_1] = mapminmax(INdataTRAIN(1,:),-1,1);       %normalise input
+[~,PSmapIn_2] = mapminmax(INdataTRAIN(2,:),-1,1);
+[~,PSmapOut] = mapminmax(OUTdataTRAIN(1,:),-1,1);       %normalise output
+Y1 = mapminmax('apply', TESTdata(:,2)', PSmapIn_1);     %apply our maps to testing data
+Y2 = mapminmax('apply', TESTdata(:,3)', PSmapIn_2);
+Input=[Y1; Y2];
 
- 
-[~,PSmap1] = mapminmax(INdataTRAIN(1,:),-1,1);                  % atliekam iejimo kintam?j? normavim?
-[~,PSmap2] = mapminmax(INdataTRAIN(2,:),-1,1);                  % atliekam iejimo kintam?j? normavim?
-[~,PSmapOut] = mapminmax(OUTdataTRAIN(1,:),-1,1);               % atliekam isejimo kintam?j? normavim?
-Y1 = mapminmax('apply', TESTdata(:,2)', PSmap1);
-Y2 = mapminmax('apply', TESTdata(:,3)', PSmap2);
-Iejimas=[Y1; Y2];
-
-    for i=1:length(TESTdata')                               % per ??jimus
-        in1=Iejimas(1,i);
-        in2=Iejimas(2,i);
-        for ii=1:neuron_cnt                      % per svorius INputo sluoksnio ir bias nepamirstam
-            in_tmp=in1*INw(ii,1)+in2*INw(ii,2)+B1w(ii);  
-            inwin(ii)=2/(1+exp(-2*in_tmp))-1;       % per tansig funkcija
-        end
-        for ih=1:length(HLw)                        % per svorius pasl?pto sluoksnio
-            h_tmp=inwin(ih)*HLw(ih);
-            inwhl(ih)=h_tmp;                        % nes perdavimo funkcija tiesine
-        end    
-        Out(i)=sum(inwhl)+B2w;                      % nes perdavimo funkcija tiesine viska sumuojam ir pridedam bias
+for i=1:length(TESTdata')                       %go throught all inputs 
+    in1=Input(1,i);
+    in2=Input(2,i);
+    for ii=1:length(INw)                        %  go thought weights of inouts
+        in_tmp=in1*INw(ii,1)+in2*INw(ii,2)+B1w(ii);  
+        inwin(ii)=2/(1+exp(-2*in_tmp))-1;       % using tansig function
     end
+    for ih=1:length(HLw)                        % with weights of hidden layer 
+        h_tmp=inwin(ih)*HLw(ih);
+        inwhl(ih)=h_tmp;                        % our transmission function is linear
+    end    
+    Out(i)=sum(inwhl)+B2w;                      % add all up and donâ€™t forget bias
+end
 
-Out_tikras=mapminmax('reverse',Out,PSmapOut);         % atliekam išejimo kintamuju atnormavima
+Out_tikras=mapminmax('reverse',Out,PSmapOut);   % normalize outputs
 
-
-
-% Br?žiam paveiksla , kur atvaizduojam abu atsakus
+%Draw figure with both outputs (1-made with net, 2- manually)
 figure, plot(Out_tikras,'-ob'),hold on, plot(y,'-*r'),grid
-legend('Atgamintas_kodo_eilute','net MATLAB struktura')
+legend('Atgamintas kodo eilute','net MATLAB struktura')
